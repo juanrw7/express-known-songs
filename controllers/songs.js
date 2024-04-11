e5dfc6a495a03c6a27580cc0f31de9565b2546c8
juanrw <juan.rw24@gmail.com>
2024-04-11 15:44:17 -0500

    complete part 3 of Dev Skills Lab

--- controllers/songs.js
+++ controllers/songs.js
@@ -16,11 +16,39 @@ function index (req, res) {
   })
 }
 
+function newSong(req, res) {
+  res.render('songs/new')
+}
 
+function create(req, res) {
+  req.body.isGood = true
+  console.log(req.body)
+  Song.create(req.body)
+  .then(song => {
+    res.redirect('/songs')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/songs')
+  })
+}
 
-
-
+function show(req,res){
+  Song.findById(req.params.songId)
+  .then(song =>{
+    res.render("songs/show",{
+      song: song
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect("/songs")
+  })
+}
 
 export {
   index,
+  newSong as new,
+  create,
+  show
 }
--- routes/songs.js
+++ routes/songs.js
@@ -1,10 +1,19 @@
 import { Router } from 'express'
 
-import * as todosCtrl from "../controllers/songs.js"
+import * as songsCtrl from "../controllers/songs.js"
 
 const router = Router()
 
-// GET localhost:3000/users
-router.get('/', todosCtrl.index)
+// GET localhost:3000/songs
+router.get('/', songsCtrl.index)
+
+// GET localhost:3000/songs/new
+router.get('/new', songsCtrl.new)
+
+// GET localhost:3000/songs/:songId
+router.get('/:songId',songsCtrl.show)
+
+// POST localhost:3000/songs
+router.post('/', songsCtrl.create)
 
 export { router }
